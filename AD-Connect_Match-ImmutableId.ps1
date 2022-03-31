4121f0e8da308be1cb04d800cb7e3f171d606ed7
iAmYoel <Yoel94@live.se>
2022-03-31 20:42:07 +0200

    First commit. Moved from other place

---
+++ AD-Connect_Match-ImmutableId.ps1
@@ -0,0 +1,25 @@
+﻿#Install-Module MSOnline
+#Connect-MsolService -Credential (Get-Credential)
+
+$ADUsers = @()
+$AzureUsers = @()
+
+Get-ADUser -Filter * -Properties ObjectGUID | sort | select Name,SamAccountName,UserPrincipalName,ObjectGUID | ForEach-Object{
+    
+    Clear-Variable UserImmutableID,MsolUser -ErrorAction SilentlyContinue
+
+    $UserImmutableID = [system.convert]::ToBase64String(([GUID]($_.ObjectGUID)).tobytearray())
+
+    IF($_.UserPrincipalName){$MsolUser = Get-MsolUser -UserPrincipalName $_.UserPrincipalName -ErrorAction SilentlyContinue}
+
+    IF($MsolUser){
+        $ADUsers += $_ | Select -Property Name,SamAccountName,UserPrincipalName,@{Name="ImmutableID";Expression={$UserImmutableID}}
+        #$MsolUser | Set-MsolUser -ImmutableId $UserImmutableID -Verbose
+    }
+
+}
+
+$ADUsers | foreach{$AzureUsers += Get-MsolUser -UserPrincipalName $_.UserPrincipalName | select -Property DisplayName,UserPrincipalName,ImmutableID}
+
+$ADUsers | Format-Table -Wrap -AutoSize
+$AzureUsers | Format-Table -Wrap -AutoSize
