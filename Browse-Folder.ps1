273411eab6a84c0f08916da61e5f9613ef7554df
iAmYoel <Yoel94@live.se>
2022-03-31 20:41:22 +0200

    Moved from other place

---
+++ Browse-Folder.ps1
@@ -0,0 +1,31 @@
+﻿# Function for browsing folder. https://code.adonline.id.au/folder-file-browser-dialogues-powershell/
+function Browse-Folder {
+    [Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+    [System.Windows.Forms.Application]::EnableVisualStyles()
+    $browse = New-Object System.Windows.Forms.FolderBrowserDialog
+    $browse.RootFolder = [System.Environment+SpecialFolder]'MyComputer'
+    $browse.ShowNewFolderButton = $true
+    $browse.Description = "Select a directory"
+
+    $loop = $true
+    while($loop)
+    {
+        if ($browse.ShowDialog() -eq "OK")
+        {
+        $loop = $false
+
+		#Insert your script here
+
+        } else
+        {
+            $res = [System.Windows.Forms.MessageBox]::Show("You clicked Cancel. Would you like to try again or use a default location?", "Select a location", [System.Windows.Forms.MessageBoxButtons]::RetryCancel)
+            if($res -eq "Cancel")
+            {
+                #Ends script
+                return
+            }
+        }
+    }
+    $browse.SelectedPath
+    $browse.Dispose()
+}
