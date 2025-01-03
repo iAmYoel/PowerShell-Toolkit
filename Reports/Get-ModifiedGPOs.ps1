1672e10619f9938781603439a52286bf391ec51d
iAmYoel <yoel.abraham@advania.se>
2025-01-03 10:03:01 +0100

    First commit

---
+++ Reports/Get-ModifiedGPOs.ps1
@@ -0,0 +1,10 @@
+﻿$importModule = Get-Module ActiveDirectory,GroupPolicy
+
+$date = (Get-Date).AddMonths(-1)
+
+$LinkedGPOs = Get-ADOrganizationalUnit -Filter * | Select-object -Property Name,DistinguishedName,@{n="LinkedGPOs";e={$_.LinkedGroupPolicyObjects | ForEach-object{$_.Substring(4,36)}}}
+
+ForEach ($GPO in $LinkedGPOs.LinkedGPOs) {
+    $LinkedOUs = $LinkedGPOs | where {$_.LinkedGPOs -contains $GPO}
+    Get-GPO -Guid $GPO | where {$_.ModificationTime.ToString('yyyy-MM') -eq $date.ToString('yyyy-MM')}| Select-object -Property DisplayName,ModificationTime,@{n="LinkedOUsName";e={$LinkedOUs.Name -join ", "}},@{n="LinkedOUsDN";e={$LinkedOUs.DistinguishedName -join ", "}}
+}
