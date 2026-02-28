1fed75bef5942d63f7b45fa2518f812a43feaaf0
Yoel Abraham <Yoel.Abraham@advania.se>
2026-02-28 16:11:09 +0100

    Update

--- Reports/Get-ModifiedGPOs.ps1
+++ Reports/Get-ModifiedGPOs.ps1
@@ -4,7 +4,10 @@ $date = (Get-Date).AddMonths(-1)
 
 $LinkedGPOs = Get-ADOrganizationalUnit -Filter * | Select-object -Property Name,DistinguishedName,@{n="LinkedGPOs";e={$_.LinkedGroupPolicyObjects | ForEach-object{$_.Substring(4,36)}}}
 
+$result = @()
 ForEach ($GPO in $LinkedGPOs.LinkedGPOs) {
     $LinkedOUs = $LinkedGPOs | where {$_.LinkedGPOs -contains $GPO}
-    Get-GPO -Guid $GPO | where {$_.ModificationTime.ToString('yyyy-MM') -eq $date.ToString('yyyy-MM')}| Select-object -Property DisplayName,ModificationTime,@{n="LinkedOUsName";e={$LinkedOUs.Name -join ", "}},@{n="LinkedOUsDN";e={$LinkedOUs.DistinguishedName -join ", "}}
+    $result += Get-GPO -Guid $GPO | where {$_.ModificationTime.ToString('yyyy-MM') -eq $date.ToString('yyyy-MM')}| Select-object -Property DisplayName,ModificationTime,@{n="LinkedOUsName";e={$LinkedOUs.Name -join ", "}},@{n="LinkedOUsDN";e={$LinkedOUs.DistinguishedName -join ", "}}
 }
+
+$result | Export-Csv -Path C:\temp\modifiedGPOs.csv -Encoding UTF8 -Delimiter ";" -NoTypeInformation -Force
